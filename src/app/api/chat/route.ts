ac2a6f2376083e70d6bbbb99239a34bde274848a
nyuta01 <nyuta2048@gmail.com>
2025-04-06 13:28:11 +0900

    feat: add mastra setup

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,26 +1,10 @@
-import { openai } from "@ai-sdk/openai";
-import { jsonSchema, streamText } from "ai";
-
-export const runtime = "edge";
-export const maxDuration = 30;
+import { mastra } from "@/mastra";
 
 export async function POST(req: Request) {
-  const { messages, system, tools } = await req.json();
+  const { messages } = await req.json();
 
-  const result = streamText({
-    model: openai("gpt-4o"),
-    messages,
-    // forward system prompt and tools from the frontend
-    system,
-    tools: Object.fromEntries(
-      Object.entries<{ parameters: unknown }>(tools).map(([name, tool]) => [
-        name,
-        {
-          parameters: jsonSchema(tool.parameters!),
-        },
-      ]),
-    ),
-  });
+  const agent = await mastra.getAgent("analyzer");
+  const result = await agent.stream(messages);
 
   return result.toDataStreamResponse();
 }
